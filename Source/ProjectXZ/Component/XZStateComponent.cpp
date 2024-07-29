105c55ceb4d465c261c7637d497d78e987e744ad
KwonYeongmin <kym60002@daum.net>
2024-07-29 22:02:20 +0900

    [권영민][XZ-17] XZStateComponent
    [ADD] XZStateComponent 추가 및 GameplayTag 추가

--- Source/ProjectXZ/Component/XZStateComponent.cpp
+++ Source/ProjectXZ/Component/XZStateComponent.cpp
@@ -1,15 +1,42 @@
 #include "XZStateComponent.h"
-
+#include "Net/UnrealNetwork.h"
+#include "ProjectXZ/GameplayTag/XZGameplayTags.h"
+#include "Character/XZCharacter.h"
+#include "Character/XZCharacter.h"
 
 UXZStateComponent::UXZStateComponent()
 {
 	PrimaryComponentTick.bCanEverTick = false;
 
+	CurrentState = FXZTags::GetXZTags().StateTag_Alive_Posture_Idle;
 }
 
 
 void UXZStateComponent::BeginPlay()
 {
 	Super::BeginPlay();
+	// :: AnimInstance Bind 
+	// Cast<UXZAnimInstnace>(GetOwner<ACharacter>()->GetMesh()->GetAnimInstance())
+}
 
+void UXZStateComponent::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+	DOREPLIFETIME(UXZStateComponent, CurrentState);
+}
+
+void UXZStateComponent::SetState(FGameplayTag NewState)
+{
+	CurrentState = NewState;
+}
+
+void UXZStateComponent::OnRep_CurrentState()
+{
+	OnChangeState();
+}
+
+void UXZStateComponent::OnChangeState()
+{
+	OnStateChanged.Broadcast(CurrentState);
 }
--- Source/ProjectXZ/GameplayTag/XZGameplayTags.cpp
+++ Source/ProjectXZ/GameplayTag/XZGameplayTags.cpp
@@ -19,6 +19,18 @@ void FXZTags::InitializeNativeGameplayTags()
 	GameplayTags.State_Equipped = UGameplayTagsManager::Get().AddNativeGameplayTag(FName(TEXT("State.Equipped")), FString("장착"));
 
 	//************************************************************************************************
+	// Character State
+	GameplayTags.StateTag_Alive_Posture_Idle = UGameplayTagsManager::Get().AddNativeGameplayTag(FName(TEXT("State.Alive.Posture.Idle")), FString("기본 상태"));
+	GameplayTags.StateTag_Alive_Posture_Crouch = UGameplayTagsManager::Get().AddNativeGameplayTag(FName(TEXT("State.Alive.Posture.Crouch")), FString("앉기 상태"));
+	GameplayTags.StateTag_Alive_Posture_Jump = UGameplayTagsManager::Get().AddNativeGameplayTag(FName(TEXT("State.Alive.Posture.Jump")), FString("점프 상태"));
+	GameplayTags.StateTag_Alive_Equip_Idle = UGameplayTagsManager::Get().AddNativeGameplayTag(FName(TEXT("State.Alive.Equip.Idle")), FString("장착 기본 상태"));
+	GameplayTags.StateTag_Alive_Equip_Aim = UGameplayTagsManager::Get().AddNativeGameplayTag(FName(TEXT("State.Alive.Equip.Aim")), FString("장착 조준 상태"));
+	GameplayTags.StateTag_Alive_Equip_Shoulder = UGameplayTagsManager::Get().AddNativeGameplayTag(FName(TEXT("State.Alive.Equip.Shoulder")), FString("장착 견착 상태"));
+	GameplayTags.StateTag_Alive_Equip_Fire = UGameplayTagsManager::Get().AddNativeGameplayTag(FName(TEXT("State.Alive.Equip.Fire")), FString("장착 발사 상태"));
+	GameplayTags.StateTag_Respawn = UGameplayTagsManager::Get().AddNativeGameplayTag(FName(TEXT("State.Respawn")), FString("리스폰 상태"));
+	GameplayTags.StateTag_Dead = UGameplayTagsManager::Get().AddNativeGameplayTag(FName(TEXT("State.Dead")), FString("죽은 자세"));
+
+	//************************************************************************************************
 	// Enhanced Input
 	GameplayTags.InputTag_Move = UGameplayTagsManager::Get().AddNativeGameplayTag(FName("InputTag.Move"), FString("이동 버튼 Input Tag"));
 	GameplayTags.InputTag_Look_Mouse = UGameplayTagsManager::Get().AddNativeGameplayTag(FName("InputTag.Look.Mouse"), FString("마우스 Input Tag"));
@@ -33,6 +45,7 @@ void FXZTags::InitializeNativeGameplayTags()
 	GameplayTags.InputTag_Weapon_Reload = UGameplayTagsManager::Get().AddNativeGameplayTag(FName("InputTag.Weapon_Reload"), FString("재장전 버튼 Input Tag"));
 	GameplayTags.InputTag_Weapon_Fire = UGameplayTagsManager::Get().AddNativeGameplayTag(FName("InputTag.Weapon_Fire"), FString("발사 버튼 Input Tag"));
 	GameplayTags.InputTag_Weapon_Aim = UGameplayTagsManager::Get().AddNativeGameplayTag(FName("InputTag.Weapon_Aim"), FString("조준 버튼 Input Tag"));
+
 
 	// Input 상태: Pressed, Held, Released
 	//GameplayTags.BlockTag_InputPressed = UGameplayTagsManager::Get().AddNativeGameplayTag(FName("BlockTag.InputPressed"), FString("Block: Input Pressed 상태"));
